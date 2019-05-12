96338db2a105ad5b94a7d38ebae40b8bfae71423
976197740@qq.com <976197740@qq.com>
2019-05-12 21:39:27 +0800

    星级评价开发

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ContentComponent } from './content/content.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { StockManageComponent } from './stock/stock-manage/stock-manage.component';
+import { StarsComponent } from './stars/stars.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { StockManageComponent } from './stock/stock-manage/stock-manage.componen
     ContentComponent,
     FooterComponent,
     SidebarComponent,
-    StockManageComponent
+    StockManageComponent,
+    StarsComponent
   ],
   imports: [
     BrowserModule,
